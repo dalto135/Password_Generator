e4b80f2428a2d66b2fd74d08269aee13300c218a
Dalton Wilkins <wilkins.271@osu.edu>
2021-02-10 00:51:16 -0500

    last commit was for a for loop, not a while loop. this commit is for tweaks

--- Develop/script.js
+++ Develop/script.js
@@ -2,15 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // Variables defined at the beginning of the function
+  // Array of arrays used to let the user choose which character types to include in their password
   var object = [["Lowercase?", "abcdefghijklmnopqrstuvwxyz"], ["Uppercase?", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"], 
   ["Numbers?", "0123456789"], ["Special characters?", "[$&+,:;=?@#|'<>.-^*()%!]"]];
-  var characters = "";
-  var type = 0;
+  
+  //While loop that takes an input from the user to be used as the password length. Must be an Integer
   var strLength = prompt("What is your password length?");
   var length = parseInt(strLength);
-
-  //While loop that takes an input from the user to be used as the password length. Must be an Integer
   while (!Number.isInteger(length) || length < 8 || length > 128) {
       strLength = prompt("Enter a number between 8 and 128");
       length = parseInt(strLength);
@@ -18,6 +16,8 @@ function generatePassword() {
 
   //While loop that displays a series of prompts to determine what type of password to create,
   //continues until at least one character type is selected
+  var characters = "";
+  var type = 0;
   while (type === 0) {
     for (i = 0; i < object.length; i++) {
       var input = prompt(object[i][0]);
@@ -32,12 +32,13 @@ function generatePassword() {
         console.log(characters);
       }
     }
+      //Ensures that at least one character type was chosen to create the password
       if (type === 0) {
         prompt("You must add at least one character type");
       }
   }
 
-  //Employes a random number generator to create the unique password
+  //Employs a random number generator to create the unique password
   var pass = "";
   for (var i = 0; i < length; i++) {
     var random = Math.floor(Math.random() * characters.length);
