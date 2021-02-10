ba2c6085e734e09557fcc994953d1898ff73ca90
Dalton Wilkins <wilkins.271@osu.edu>
2021-02-10 00:25:26 -0500

    created a while loop to radically simplify code by eliminating repeating lines

--- Develop/script.js
+++ Develop/script.js
@@ -2,79 +2,51 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    var strLength = prompt("What is your password length?");
-    var length = parseInt(strLength);
+  // Variables defined at the beginning of the function
+  var object = [["Lowercase?", "abcdefghijklmnopqrstuvwxyz"], ["Uppercase?", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"], 
+  ["Numbers?", "0123456789"], ["Special characters?", "[$&+,:;=?@#|'<>.-^*()%!]"]];
+  var characters = "";
+  var type = 0;
+  var strLength = prompt("What is your password length?");
+  var length = parseInt(strLength);
+
+  //While loop that takes an input from the user to be used as the password length. Must be an Integer
   while (!Number.isInteger(length) || length < 8 || length > 128) {
       strLength = prompt("Enter a number between 8 and 128");
       length = parseInt(strLength);
   }
 
-  //while loop that displaces a series of prompts to determine what type of password to create,
+  //While loop that displays a series of prompts to determine what type of password to create,
   //continues until at least one character type is selected
-  var characters = "";
-  var type = 0;
   while (type === 0) {
-      var lowercase = prompt("Lowercase?");
-      var capLowercase = lowercase.toUpperCase();
-      while (capLowercase !== "YES" && capLowercase !== "NO") {
-          lowercase = prompt("Must answer yes or no");
-          capLowercase = lowercase.toUpperCase();
+    for (i = 0; i < object.length; i++) {
+      var input = prompt(object[i][0]);
+      var capInput = input.toUpperCase();
+      while (capInput !== "YES" && capInput !== "NO") {
+        input = prompt("Must answer yes or no");
+        capInput = input.toUpperCase();
       }
-      if (capLowercase === "YES") {
-          characters += "abcdefghijklmnopqrstuvwxyz";
-          type++;
-          console.log(characters);
+      if (capInput === "YES") {
+        characters += object[i][1];
+        type++;
+        console.log(characters);
       }
-
-      var uppercase = prompt("Uppercase?");
-      var capUppercase = uppercase.toUpperCase();
-      while (capUppercase !== "YES" && capUppercase !== "NO") {
-          uppercase = prompt("Must answer yes or no");
-          capUppercase = uppercase.toUpperCase();
-      }
-      if (capUppercase === "YES") {
-          characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          type++;
-          console.log(characters);
-      }
-
-        var numeric = prompt("Numbers?");
-        var capNumeric = numeric.toUpperCase();
-      while (capNumeric !== "YES" && capNumeric !== "NO") {
-          numeric = prompt("Must answer yes or no");
-          capNumeric = numeric.toUpperCase();
-      }
-      if (capNumeric === "YES") {
-          characters += "0123456789";
-          type++;
-          console.log(characters);
-      }
-
-        var special = prompt("Special characters?");
-        var capSpecial = special.toUpperCase();
-      while (capSpecial !== "YES" && capSpecial !== "NO") {
-          special = prompt("Must answer yes or no");
-          capSpecial = special.toUpperCase();
-      }
-      if (capSpecial === "YES") {
-          characters += "[$&+,:;=?@#|'<>.-^*()%!]";
-          type++;
-          console.log(characters);
-      }
+    }
       if (type === 0) {
         prompt("You must add at least one character type");
       }
-    }
-  var password = "";
-//Employes a random number generator to create the unique password
+  }
+
+  //Employes a random number generator to create the unique password
+  var pass = "";
   for (var i = 0; i < length; i++) {
     var random = Math.floor(Math.random() * characters.length);
     var cha = characters.charAt(random);
-    password += cha;
+    pass += cha;
   }
-  console.log("Password: " + password);
+  console.log("Password: " + pass);
 
-  return password;
+  return pass;
 }
 
 // Write password to the #password input
