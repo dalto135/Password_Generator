d6878e42690caa8daa8a3b84caf3e61b50b6048f
dalto135 <67397294+dalto135@users.noreply.github.com>
2022-04-03 23:25:31 -0400

    passwords can now be customized with on screen settings instead of prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,11 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 var quickGenerate = document.querySelector("#quickGenerate");
 var copyButton = document.querySelector("#copyButton");
 
-function generatePassword() {
-  // Array of arrays used to let the user choose which character types to include in their password
-  var array = [["Lowercase?", "abcdefghijklmnopqrstuvwxyz"], ["Uppercase?", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"], 
-  ["Numbers?", "0123456789"], ["Special characters?", "!\"#$%&'()*+,-./:;<=>?@_`{|}~^"]];
-  
-  //Do...while loop that takes an input from the user to be used as the password length. Must be an Integer
-  do {
-    var strLength = prompt("What is your password length? (Must be between 8 and 128 characters)");
-    var length = parseInt(strLength);
-    console.log(strLength);
-
-    if (strLength === null) {
-      console.log("exit");
-      return;
-    }
-
-    var numbers = true;
-
-    for (let char of strLength) {
-      if (!Number.isInteger(parseInt(char))) {
-        numbers = false;
-      }
-    }
-  } while (length < 8 || length > 128 || !numbers || strLength === "");
-
-  //While loop that displays a series of prompts to determine what type of password to create,
-  //continues until at least one character type is selected
-  var characters = "";
-  var type = 0;
-  while (type === 0) {
-    for (i = 0; i < array.length; i++) {
-      var input = confirm(array[i][0]);
-
-      if (input === true) {
-        characters += array[i][1];
-        type++;
-        console.log(characters);
-      }
-    }
-      //Ensures that at least one character type was chosen to create the password
-      if (type === 0) {
-        alert("You must add at least one character type");
-      }
-  }
-
-  //Employs a random number generator to create the unique password
-  var pass = constructPassword(characters, length);
-
-  return pass;
-}
-
-function generateQuickPassword() {
-  var quickCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@_`{|}~^";
-  var length = 16;
-
-  var quickPass = constructPassword(quickCharacters, length);
-
-  return quickPass;
-}
-
 function constructPassword(characters, length) {
   var password = "";
+
+  console.log("length: " + length);
+
   for (var i = 0; i < length; i++) {
     var random = Math.floor(Math.random() * characters.length);
     var cha = characters.charAt(random);
@@ -74,50 +15,81 @@ function constructPassword(characters, length) {
   return password;
 }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  if (password) {
-    passwordText.value = password;
+function checkLength(length) {
+  for (let char of length) {
+    if (!Number.isInteger(parseInt(char))) {
+      return false;
+    }
   }
+
+  if (parseInt(length) < 8 || parseInt(length) > 128) {
+    return false;
+  }
+
+  return parseInt(length);
 }
 
 function writeQuickPassword() {
-  do {
-    var lower = false;
-    var upper = false;
-    var number = false;
-    var symbol = false;
+  const lowerCheck = document.querySelector("#lowerCheck");
+  const upperCheck = document.querySelector("#upperCheck");
+  const numberCheck = document.querySelector("#numberCheck");
+  const symbolCheck = document.querySelector("#symbolCheck");
 
-    var quickPassword = generateQuickPassword();
-    console.log("quickPassword: " + quickPassword);
+  const passwordLength = document.querySelector("#passwordLength");
 
-    for (let char of quickPassword) {
-      if (char.match(/[a-z]/i) != null && char == char.toLowerCase()) {
-        lower = true;
-        console.log('LOWER');
-      }
-      else if (char.match(/[a-z]/i) != null && char == char.toUpperCase()) {
-        upper = true;
-        console.log('UPPER');
-      }
-      else if (Number.isInteger(parseInt(char))) {
-        number = true;
-        console.log('NUMBER');
-      }
-      else {
-        symbol = true;
-        console.log('SYMBOL');
-      }
-    }
-  } while (!lower || !upper || !number || !symbol);
+  var characters = "";
 
-  console.log("FINAL PASSWORD: " + quickPassword);
+  if (lowerCheck.checked) {
+    characters += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (upperCheck.checked) {
+    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (numberCheck.checked) {
+    characters += "0123456789";
+  }
+  if (symbolCheck.checked) {
+    characters += "!\"#$%&'()*+,-./:;<=>?@_`{|}~^";
+  }
+  console.log("characters: " + characters);
 
-  var quickPasswordText = document.querySelector("#password");
-  quickPasswordText.value = quickPassword;
+  var cleanLength = checkLength(passwordLength.value);
+
+  if (characters != "" && cleanLength) {
+    do {
+      var lower = false;
+      var upper = false;
+      var number = false;
+      var symbol = false;
+
+      var quickPassword = constructPassword(characters, cleanLength);
+      console.log("password: " + quickPassword);
+
+      for (let char of quickPassword) {
+        if (char.match(/[a-z]/i) != null && char == char.toLowerCase()) {
+          lower = true;
+          console.log('LOWER');
+        }
+        else if (char.match(/[a-z]/i) != null && char == char.toUpperCase()) {
+          upper = true;
+          console.log('UPPER');
+        }
+        else if (Number.isInteger(parseInt(char))) {
+          number = true;
+          console.log('NUMBER');
+        }
+        else {
+          symbol = true;
+          console.log('SYMBOL');
+        }
+      }
+    } while (lower != lowerCheck.checked || upper != upperCheck.checked || number != numberCheck.checked || symbol != symbolCheck.checked);
+  
+    console.log("FINAL PASSWORD: " + quickPassword);
+
+    var quickPasswordText = document.querySelector("#password");
+    quickPasswordText.value = quickPassword;
+  }
 }
 
 async function copyText() {
@@ -137,9 +109,5 @@ async function copyText() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 quickGenerate.addEventListener("click", writeQuickPassword);
 copyButton.addEventListener("click", copyText);
-
-// quickGenerate.addEventListener("click", copyText);
-// generateBtn.addEventListener("click", copyText);
